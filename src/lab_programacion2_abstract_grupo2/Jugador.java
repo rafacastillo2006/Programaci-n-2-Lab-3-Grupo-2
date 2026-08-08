7411bc69b80acc1a690a99419b6a1cedcf67b90e
rafacastillo2006 <raficastillo2006@gmail.com>
2026-08-08 09:21:14 -0600

    Actualizar file de clase jugador

--- src/lab_programacion2_abstract_grupo2/Jugador.java
+++ src/lab_programacion2_abstract_grupo2/Jugador.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lab_programacion2_abstract_grupo2;
 
+package lab_programacion2_abstract_grupo2;
 /**
  *
  * @author Rafa
  */
 public class Jugador {
+   
+    private String nombre;
+    private int puntosPartida;
+    private long tiempoAcumulado;
+    private long inicioTurno;
     
+    public Jugador(){
+        this.nombre = nombre;
+        this.puntosPartida = 0;
+        this.tiempoAcumulado = 0;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntosPartida() {
+        return puntosPartida;
+    }
+
+    public void sumarAcierto() {
+        this.puntosPartida++;
+    }
+    
+    public void iniciarTurno(){
+        this.inicioTurno = System.currentTimeMillis();
+    }
+    
+    public void pausaTurno(){
+        if(this.inicioTurno > 0){
+            this.tiempoAcumulado += (System.currentTimeMillis() - this.inicioTurno);
+        }this.inicioTurno=0;
+    }
+    
+    public long getTiempoEnSegundos(){
+        return tiempoAcumulado / 1000;
+    }
 }
