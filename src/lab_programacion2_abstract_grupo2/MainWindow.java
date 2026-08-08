0f7f1cdbe7f726ccecfd77bbcbbcd890a679e528
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-08 09:51:43 -0600

    Agregar clases mainwindow y pantalla config

---
+++ src/lab_programacion2_abstract_grupo2/MainWindow.java
@@ -0,0 +1,45 @@
+
+package lab_programacion2_abstract_grupo2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame{
+    
+    public static final String pantalla_config = "Configuracion";
+    public static final String pantalla_juego = "Juego";
+    
+    private CardLayout cardLayout;
+    private JPanel panelContenedor;
+    //private PantallaJuego pantallaJuego;
+    
+    public MainWindow(){
+        setTitle("Juego De Memoria Pokemon");
+        setBackground(Color.gray);
+        setSize(700, 700);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        cardLayout = new CardLayout();
+        panelContenedor = new JPanel(cardLayout);
+        PantallaConfiguracion pantallaConfiguracion = new PantallaConfiguracion(this);
+        //pantallaJuego = new PantallaJuego();
+        
+        panelContenedor.add(pantallaConfiguracion, pantalla_config);
+        //panelContenedor.add(pantallaJuego, pantalla_juego);
+        add(panelContenedor);
+        cardLayout.show(panelContenedor, pantalla_config);
+
+        setVisible(true);
+    }
+    
+    public void mostrarPantalla(String nombrePantalla){
+    cardLayout.show(panelContenedor, nombrePantalla);
+    }
+    
+    public void iniciarJuego(String nombrep1, String nombrep2){
+   // pantallaJuego.configurarJugadores(nombrep1, nombrep2);
+        mostrarPantalla(pantalla_juego);
+    }
+}
---
+++ src/lab_programacion2_abstract_grupo2/PantallaConfiguracion.java
@@ -0,0 +1,81 @@
+
+package lab_programacion2_abstract_grupo2;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class PantallaConfiguracion extends JPanel{
+    
+    private final MainWindow ventanaPrincipal;
+    
+    private JTextField campoJugador1;
+    private JTextField campoJugador2;
+    private JButton botonJugar;
+    
+    public PantallaConfiguracion(MainWindow ventanaPrincipal){
+    this.ventanaPrincipal = ventanaPrincipal;
+    configurarPanel();
+    }
+    
+    private void configurarPanel(){
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        JLabel titulo = new JLabel("Juego de Memoria Pokemon", SwingConstants.CENTER);
+        titulo.setFont(new Font("SansSerif", Font.BOLD, 24));
+        
+        JLabel labelJugador1 = new JLabel("Nombre del jugador 1");
+        campoJugador1 = new JTextField(15);
+        JLabel labelJugador2 = new JLabel("Nombre del jugador 2");
+        campoJugador2 = new JTextField(15);
+        botonJugar = new JButton("Jugar!");
+        botonJugar.addActionListener(e -> {
+        
+        String nombreJugador1 = campoJugador1.getText().trim();
+        String nombreJugador2 = campoJugador2.getText().trim();
+        String mensajeError = validarNombres(nombreJugador1, nombreJugador2);
+            if (mensajeError!=null) {
+                JOptionPane.showMessageDialog(this, mensajeError, "Datos incompletos", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            
+            ventanaPrincipal.iniciarJuego(nombreJugador1, nombreJugador2);
+            campoJugador1.setText("");
+            campoJugador2.setText("");
+            
+        });
+        
+        //gbc 
+         gbc.gridy = 0;
+        add(titulo, gbc);
+        gbc.gridy = 1;
+        add(labelJugador1, gbc);
+        gbc.gridy = 2;
+        add(campoJugador1, gbc);
+        gbc.gridy = 3;
+        add(labelJugador2, gbc);
+        gbc.gridy = 4;
+        add(campoJugador2, gbc);
+        gbc.gridy = 5;
+        add(botonJugar, gbc);
+        
+    }
+    private String validarNombres(String nombreJugador1, String nombreJugador2){
+    boolean faltaP1 = nombreJugador1.isEmpty();
+    boolean faltaP2 = nombreJugador2.isEmpty();
+    
+        if (faltaP1 && faltaP2) {
+            return "Debes ingresar el nombre de los 2 jugadores";
+        } else if (faltaP1) {
+            return "Falta ingresar el nombre de jugador 1";
+        } else if (faltaP2) {
+            return "Falta ingresar el nombre de jugador 2";
+        }
+        return null;
+    }
+    
+    
+}
